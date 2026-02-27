1e81316f6870da0f9395888d4ddcfa4d4315915c
nahtanoj489 <nahtanoj489@gmail.com>
2026-02-26 19:08:32 -0500

    api.js

--- api.js
+++ api.js
@@ -1,13 +1,74 @@
-const DB_URL =
-"https://raw.githubusercontent.com/nahtanoj489/verificacion-medicina-y-salud/main/db.json";
+const OWNER = "nahtanoj489";
+const REPO = "verificacion-medicina-y-salud";
+const FILE = "db.json";
+const BRANCH = "main";
+
+
+const TOKEN = "PEGAR_AQUI_TU_TOKEN";
+
+
+async function getDB(){
+
+const response = await fetch(
+`https://api.github.com/repos/${OWNER}/${REPO}/contents/${FILE}?ref=${BRANCH}`
+);
+
+const data = await response.json();
+
+const content = atob(data.content);
+
+return {
+
+sha:data.sha,
+data:JSON.parse(content)
+
+};
+
+}
+
+
+
+async function guardarCertificado(cert){
+
+const db = await getDB();
+
+db.data.push(cert);
+
+const newContent = btoa(
+JSON.stringify(db.data,null,2)
+);
+
+await fetch(
+`https://api.github.com/repos/${OWNER}/${REPO}/contents/${FILE}`,
+{
+method:"PUT",
+
+headers:{
+Authorization:`Bearer ${TOKEN}`,
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+
+message:"Nuevo certificado",
+
+content:newContent,
+
+sha:db.sha,
+
+branch:BRANCH
+
+})
+});
+
+}
+
 
 
 async function verificarCertificado(code){
 
-const response = await fetch(DB_URL);
+const db = await getDB();
 
-const data = await response.json();
-
-return data.filter(cert => cert.code === code);
+return db.data.filter(c=>c.code===code);
 
 }
